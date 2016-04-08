5e36c81dbe3bfd1300b123e70884b2a74099f3f2
Kareem Janoudi <kjanoudi@gmail.com>
2016-04-08 02:13:55 -0400

    fix(aws): updating aws stuff

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,5 +1,5 @@
 export default {
     env: 'production',
-    db: 'mongodb://ip-172-31-15-205.ec2.internal/leadgrabr-prod',
+    db: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ip-172-31-15-205.ec2.internal/leadgrabr-prod`,
     port: 3000
 }
