fec42a5a0447a39608cd143166dd20ab593a5201
Kareem Janoudi <kjanoudi@gmail.com>
2016-04-21 01:19:39 -0400

    issue(3): remove console logs

--- src/components/twilio/handlers/inboundAudience.js
+++ src/components/twilio/handlers/inboundAudience.js
@@ -4,10 +4,8 @@ import { subscriptionType } from 'helpers/constants'
 import _ from 'lodash'
 
 async function handleBlacklist(number, twiml) {
-    console.log('checking blacklist')
     const blacklisted = await TwilioBlacklist.find({ number })
     if (blacklisted && blacklisted.length > 0) {
-        console.log('blacklisted: ', blacklisted)
         twiml.reject()
         return true
     }
@@ -15,10 +13,8 @@ async function handleBlacklist(number, twiml) {
 }
 
 async function getAudience(audienceId, twiml) {
-    console.log('finding audience')
     const audience = await Audience.findById(audienceId)
     if (!audience) {
-        console.log('audience not found for this number') // in future, log this as an error
         twiml.reject()
         return null
     }
@@ -30,27 +26,22 @@ async function getSubscriberPhoneNumbers(audience) {
         audience: audience._id,
         subscriptionType: subscriptionType.Phone
     })
-    console.log('subs: ', subscribers)
     return subscribers.map((sub) => _.get(sub, 'deliverTo.phone'))
 }
 
 export default (router) => {
     router.post('/call/:audienceId', async (req, res) => {
-        console.log('Call: ', req.body)
         const twiml = new twilio.TwimlResponse()
         const blacklist = await handleBlacklist(req.body.from, twiml)
         if (blacklist) {
-            console.log('blacklisted, returning')
             return res.xml(twiml.toString())
         }
         const audience = await getAudience(req.params.audienceId, twiml)
         if (!audience) {
-            console.log('no audience, returning')
             return res.xml(twiml.toString())
         }
         const numbers = await getSubscriberPhoneNumbers(audience)
         if (!numbers || numbers.length === 0) {
-            console.log('no subscribers, returning')
             twiml.reject()
             return res.xml(twiml.toString())
         }
