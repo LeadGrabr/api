a5066a3d8a3da370c5d921e64be886f344d410e6
Kareem Janoudi <kjanoudi@gmail.com>
2016-04-30 18:54:57 -0400

    master(phone): validator update

--- src/helpers/customValidators.js
+++ src/helpers/customValidators.js
@@ -1,13 +1,13 @@
 import Joi from 'joi'
 import Hoek from 'hoek'
 
-const phoneNumberPattern = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/gi // eslint-disable-line max-len
+const phoneNumberPattern = /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/gi // eslint-disable-line max-len
 
 export const phone = Hoek.clone(Joi.string().regex(phoneNumberPattern).options({
     language: {
         string: {
             regex: {
-                base: '"{{!value}}" doesn\'t look like a valid US phone number, such as: (242) 333-5555' // eslint-disable-line max-len
+                base: '"{{!value}}" doesn\'t look like a valid US phone number, such as: 248-333-5555' // eslint-disable-line max-len
             }
         }
     }
